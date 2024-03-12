779af64cd0a4bc9c8ad59ada17a2f857c523dc71
savannaretro <ulastanersoyak@hotmail.com>
2024-03-12 12:32:55 +0300

    slam toolbox, rviz config

--- src/camera/src/camera_capture.cpp
+++ src/camera/src/camera_capture.cpp
@@ -38,7 +38,6 @@ public:
     RCLCPP_INFO(this->get_logger(), "edge detector is up");
   }
 };
-
 int main(int argc, char **argv) {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<camera_node>());
